fde78da9fa5dc21d8bb737c33226efa1a5c832cb
ksu930 <ksu930310@gmail.com>
2022-12-14 01:15:51 +0900

    api 변경

--- src/api/index.js
+++ src/api/index.js
@@ -1,39 +1,41 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://13.124.216.20:8080/api",
-})
+  baseURL: "http://3.38.101.115:8080/api",
+});
 
 api.interceptors.request.use(
-    function (config) {
-      if (
-        config.url !== "login" ||
-        config.url !== "signup" ||
-        config.url !== "id-duplicate" ||
-        config.url !== "name-duplicate" ||
-        config.url !== "best"
-        ) {config.headers.Authorization = `Bearer ${sessionStorage.getItem("Access_Token")}`}
-      return config;
-    },
-    function (error) {
-      return Promise.reject(error);
+  function (config) {
+    if (
+      config.url !== "login" ||
+      config.url !== "signup" ||
+      config.url !== "id-duplicate" ||
+      config.url !== "name-duplicate" ||
+      config.url !== "best"
+    ) {
+      config.headers.Authorization = `Bearer ${sessionStorage.getItem(
+        "Access_Token"
+      )}`;
     }
-  );
-  
-  // api요청 주소가 login일 경우 access token과 refresh token을 받아 로컬스토리지에 저장해준다.
-  api.interceptors.response.use(
-    function (response) {
-      if (response.config.url === "/login" && response.data.success) {
-        localStorage.setItem("accessToken", response.headers.accessToken);
-        localStorage.setItem("refreshToken", response.headers.refreshToken);
-      }
-      return response;
-    },
-    function (error) {
-      return Promise.reject(error.response.data.error);
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+// api요청 주소가 login일 경우 access token과 refresh token을 받아 로컬스토리지에 저장해준다.
+api.interceptors.response.use(
+  function (response) {
+    if (response.config.url === "/login" && response.data.success) {
+      localStorage.setItem("accessToken", response.headers.accessToken);
+      localStorage.setItem("refreshToken", response.headers.refreshToken);
     }
-  );
-  
+    return response;
+  },
+  function (error) {
+    return Promise.reject(error.response.data.error);
+  }
+);
 
-
-  export default api;
+export default api;
